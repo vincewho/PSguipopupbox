3352d4b2d691f48e6fd3575b75f248ab6f04d59f
Vincent Li <vincent.li@krostcpas.com>
2020-05-09 15:55:08 -0700

    first

---
+++ popup.ps1
@@ -0,0 +1,38 @@
+<# This form was created using POSHGUI.com  a free online gui designer for PowerShell
+.NAME
+    test_popup
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = '600,200'
+$Form.text                       = "Form"
+$Form.TopMost                    = $false
+
+$Button1                         = New-Object system.Windows.Forms.Button
+$Button1.text                    = "Prompt Box"
+$Button1.width                   = 100
+$Button1.height                  = 30
+$Button1.location                = New-Object System.Drawing.Point(25,20)
+$Button1.Font                    = 'Microsoft Sans Serif,10'
+
+$output_lbl                      = New-Object system.Windows.Forms.Label
+$output_lbl.text                 = "output_lbl"
+$output_lbl.AutoSize             = $true
+$output_lbl.width                = 300
+$output_lbl.height               = 10
+$output_lbl.location             = New-Object System.Drawing.Point(25,80)
+$output_lbl.Font                 = 'Microsoft Sans Serif,10'
+
+$Form.controls.AddRange(@($Button1,$output_lbl))
+
+$Button1.Add_Click({ get-prompt })
+
+function get-prompt { }
+
+
+#Write your logic code here
+
+[void]$Form.ShowDialog()
